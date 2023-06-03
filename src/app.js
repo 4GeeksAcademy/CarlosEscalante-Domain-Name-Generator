948b25a7c1666c8d58eb2505231919e73a0c0076
Carlos Eduardo Escalante Montilla <129804004+CarlosEscalanteM@users.noreply.github.com>
2023-06-03 00:32:38 +0000

    first attempt

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our", "his", "my"];
+let adj = ["great", "big", "fantastic", "rational"];
+let noun = ["jogger", "racoon", "chipmunk", "walrus"];
+let at = [".com", ".edu", "io", ".gov"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < at.length; l++) {
+        console.log(`${pronoun[i]}${adj[j]}${noun[k]}${at[l]}`);
+      }
+    }
+  }
+}
